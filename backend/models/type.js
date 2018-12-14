7698c7613cc09c80a8e59d7b2915fc9f072db470
nortelli <nortelli@debmedia.com>
2018-12-14 13:57:12 -0300

    model type

--- backend/models/type.js
+++ backend/models/type.js
@@ -1,3 +1,12 @@
 var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
+
+var TypeSchema = new Type(
+  {
+    name: {type: String, required: true, unique: true}
+  }
+);
+
+//Export model
+module.exports = mongoose.model('Type', TypeSchema);
