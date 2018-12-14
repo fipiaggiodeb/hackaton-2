2d9322ccaab542f8b87d2cbd3673d9467b356c3f
Matias Lopez <mnlopez@debmedia.com>
2018-12-14 14:12:09 -0300

    product.js

--- backend/models/product.js
+++ backend/models/product.js
@@ -1,3 +1,16 @@
 var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
+
+var ProductSchema = new Schema(
+	{
+		name: {type: String, required: true},
+		sku:  {type: String, required: true, unique: true},
+		amount: {type: String, required: true},
+		image: {type: String, required:true},
+		category: {type: Schema.Types.ObjectId, ref:'Category', required: true} 
+	}
+);
+
+//Export model
+module.exports = mongoose.model('Product', UserSchema);
