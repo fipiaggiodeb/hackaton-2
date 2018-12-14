f7bdb0f9b796573ca488aa97fffc2e4411c368fb
nortelli <nortelli@debmedia.com>
2018-12-14 14:09:55 -0300

    type => category

--- backend/models/category.js
+++ backend/models/category.js
@@ -2,11 +2,11 @@ var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
 
-var TypeSchema = new Type(
+var CategorySchema = new Type(
   {
     name: {type: String, required: true, unique: true}
   }
 );
 
 //Export model
-module.exports = mongoose.model('Type', TypeSchema);
+module.exports = mongoose.model('Category', CategorySchema);
