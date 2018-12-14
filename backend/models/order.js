c2eb9dfc089531bee000c4f7e18df28f9dc0cf64
nortelli <nortelli@debmedia.com>
2018-12-14 14:19:26 -0300

    model order

--- backend/models/order.js
+++ backend/models/order.js
@@ -1,3 +1,16 @@
 var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
+
+var OrderSchema = new Schema(
+  {
+    date: 		{type: Date, default: Date.now, required: true},
+    sender: 	{type: String, required: true},
+    recipient: 	{type: String, required: true},
+    comments: 	{type: String, required: false},
+    products: 	[ProductSchema]
+  }
+);
+
+//Export model
+module.exports = mongoose.model('Order', OrderSchema);
