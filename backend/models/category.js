d46e052a42692a04a92dd701c83dfad86b1073a2
nortelli <nortelli@debmedia.com>
2018-12-14 14:30:03 -0300

    fix category

--- backend/models/category.js
+++ backend/models/category.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
 
-var CategorySchema = new Type(
+var CategorySchema = new Schema(
   {
     name: {type: String, required: true, unique: true}
   }
